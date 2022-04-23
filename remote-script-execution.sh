49dc5f0707b097cba197b563b99f8d1a00ea6093
strangedin <36398275+strangedin@users.noreply.github.com>
2022-04-24 01:47:01 +0530

    Add files via upload

---
+++ remote-script-execution.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#install sshpass
+sudo apt-get update -y
+sudo apt-get install -y sshpass
+
+#take inputs
+read -p "Enter username:" username
+read -p "Enter password:" password
+read -p "Enter ipaddress:" ip_address
+
+#access remote device and executes script
+sshpass -p $password ssh -o StrictHostKeyChecking=no $username@$ip_address <<EOF 
+sudo apt update -y  
+sudo apt upgrade -y
+EOF
+#Mention the scripts that you want to execute <<EOF "script" EOF 
