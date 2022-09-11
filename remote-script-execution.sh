55685a5048a9103e98d2f589cabc5d79cc0c0790
strangedin <36398275+strangedin@users.noreply.github.com>
2022-09-11 21:37:38 +0530

    Update remote-script-execution.sh

--- remote-script-execution.sh
+++ remote-script-execution.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-#install sshpass
+#installs sshpass
 sudo apt-get update -y
 sudo apt-get install -y sshpass
 
-#take inputs
+#takes inputs
 read -p "Enter username:" username
 read -p "Enter password:" password
 read -p "Enter ipaddress:" ip_address
 
 #access remote device and executes script
 sshpass -p $password ssh -o StrictHostKeyChecking=no $username@$ip_address <<EOF 
-sudo apt update -y  
-sudo apt upgrade -y
+#Mention the scripts here that you want to execute on the remote device
 EOF
-#Mention the scripts <<EOF "script" EOF that you want to execute on the remote device
+
